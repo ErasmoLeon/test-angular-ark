c24c138846f8e4464751bd2eb34c25ebec4fb71d
Erasmo Leon Lopez <erasmo.leon@globant.com>
2019-01-28 17:07:50 -0600

    wip config

---
+++ src/store/reducers/user.reducers.ts
@@ -0,0 +1,24 @@
+import { EUserActions, UserActions } from '../actions/user.actions';
+import { initialUserState, IUserState } from '../state/user.state';
+
+export const userReducers = (
+  state = initialUserState,
+  action: UserActions,
+): IUserState => {
+  switch (action.type) {
+    case EUserActions.GetUsersSuccess: {
+      return {
+        ...state,
+        users: action.payload
+      };
+    }
+    case EUserActions.GetUserSuccess: {
+      return {
+        ...state,
+        selectedUser: action.payload
+      };
+    }
+    default:
+      return state;
+  }
+}
--- src/store/state/app.state.ts
+++ src/store/state/app.state.ts
@@ -1,5 +1,21 @@
 import { RouterReducerState } from '@ngrx/router-store';
 
 import { IUserState, initialUserState } from './user.state';
+import { IConfigState, initialConfigState } from './config.state';
+
+export interface IAppState {
+  router?: RouterReducerState;
+  users: IUserState;
+  config: IConfigState;
+}
+
+export const initialAppState: IAppState = {
+  users: initialUserState,
+  config: initialConfigState,
+};
+
+export function getInitialState(): IAppState {
+  return initialAppState;
+}
 
 // https://medium.com/frontend-fun/angular-ngrx-a-clean-and-clear-introduction-4ed61c89c1fc

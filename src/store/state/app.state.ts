f3d8e58015d2393d8e2f964de045aca591d73ece
Erasmo Leon Lopez <erasmo.leon@globant.com>
2019-01-28 16:05:44 -0600

    first commit

---
+++ src/store/state/app.state.ts
@@ -0,0 +1,5 @@
+import { RouterReducerState } from '@ngrx/router-store';
+
+import { IUserState, initialUserState } from './user.state';
+
+// https://medium.com/frontend-fun/angular-ngrx-a-clean-and-clear-introduction-4ed61c89c1fc
---
+++ src/store/state/config.state.ts
@@ -0,0 +1,9 @@
+import { IConfig } from '../../models/config.interface';
+
+export interface IConfigState {
+  config: IConfig;
+}
+
+export const initialConfigState: IConfigState = {
+  config: null,
+};
